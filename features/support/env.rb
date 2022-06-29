8764b13bf47e6cf5420cdde13ee71ee7ebdd917a
Maxime Noel <maxime.noel@suse.com>
2022-06-29 13:59:10 +1200

    Increase timeout to 15

--- features/support/env.rb
+++ features/support/env.rb
@@ -3,6 +3,7 @@ require 'capybara'
 require 'capybara/dsl'
 require 'rspec'
 Capybara.run_server = false
+Capybara.default_max_wait_time = 15
 #Set default driver as Selenium
 Capybara.default_driver = :selenium
 #Set default selector as css

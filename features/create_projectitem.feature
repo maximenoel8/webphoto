909ef89501737fb70fe3be856ccbafb65b0845c6
Maxime Noel <maxime.noel@suse.com>
2022-07-01 12:46:09 +1200

    Update create project with the file configuration

---
+++ features/create_projectitem.feature
@@ -0,0 +1,29 @@
+Feature: Create new project item
+
+  Scenario: Log in as admin user
+    Given I am authorized admin user
+    
+  Scenario: Create project item and configure basics
+    When I create a new project item
+    Then I change the project title
+    And I select the work type
+    And I change 'Image mise en avant' of the project item
+    And I change page style to edge to edge
+    And I change gallery thumbnail link type to direct link lightbox
+    And I switch to page builder mode
+    And I import Wanaka block
+    
+  Scenario: Edit hero image block
+    When I open the edit page hero image block
+    Then I update the hero image block picture
+    And I change the hero image block title
+    And I register the changes from the block
+    
+  Scenario: Add pictures to thumbnails grid
+    When I open the thumbnails grid block
+    Then I open add images tab
+    And I clean the images in thumbnails grid
+    And I add to the gallery my previously imported pictures with configure legend
+
+  Scenario: Publish the new project item page
+    And I publish the page
--- features/step_definitions/projectitemsteps.rb
+++ features/step_definitions/projectitemsteps.rb
@@ -8,18 +8,18 @@ When(/^I create a new project item$/) do
   visit "#{SITE}/post-new.php?post_type=mtheme_portfolio"
 end
 
-Then(/^I change the project title by "([^"]*)"$/) do | title |
-  fill_in('title', with: title)
+Then(/^I change the project title$/) do
+  fill_in('title', with: $website_configuration['project_items']['title'])
 end
 
-And(/^I select the work type "([^"]*)"$/) do | worktype |
-  find(:xpath, "//label[@class='selectit' and contains(.,'#{worktype}')]").click()
+And(/^I select the work type$/) do
+  find(:xpath, "//label[@class='selectit' and contains(.,'#{$website_configuration['work_type']['name']}')]").click
 end
 
-And(/^I change Image mise en avant with "([^"]*)"$/) do | image |
+And(/^I change 'Image mise en avant' of the project item$/) do
   click_on_visible_element("//a[@id='set-post-thumbnail']")
   wait_spinner
-  fill_in_visible_element("//input[@id='media-search-input']", image)
+  fill_in_visible_element("//input[@id='media-search-input']", $website_configuration['project_items']['image'])
   wait_spinner
   click_on_visible_element("//li[@class='attachment save-ready']")
   click_on_visible_element("//button[text()='Définir l’image mise en avant']")
@@ -36,36 +36,36 @@ And(/^I change gallery thumbnail link type to direct link lightbox$/) do
 end
 
 And(/^I switch to page builder mode$/) do
-  find(:xpath,"//span[@class='mtheme-pb-choice mtheme-pb-yes']").click()
+  find(:xpath, "//span[@class='mtheme-pb-choice mtheme-pb-yes']").click
   check_text_present('Using Page Builder.')
 end
 
 And(/^I import Wanaka block$/) do
   #clear
-  find(:xpath,"//a[@class='emptyTemplates']").click()
+  find(:xpath, "//a[@class='emptyTemplates']").click
 
   #import block
-  find(:id,'import-a-block').click()
-  find(:xpath,"//div[@id='mtheme-pb-import-a-block']//textarea").fill_in with: BLOCK_IMPORT
-  find(:xpath,"//button[@class='button button-primary' and text()='Import']").click()
+  find(:id, 'import-a-block').click
+  find(:xpath, "//div[@id='mtheme-pb-import-a-block']//textarea").fill_in with: BLOCK_IMPORT
+  find(:xpath, "//button[@class='button button-primary' and text()='Import']").click
 end
 
 When(/^I open the edit page hero image block$/) do
   # Open Hero image block edit table
-  find(:xpath, "//li[@title='Add a Hero Image' and contains(@class,'ui-sortable-handle')]").click()
-  find(:xpath, "//li[@title='Add a Hero Image' and contains(@class,'ui-sortable-handle')]//a[@class='block-edit']").click()
+  find(:xpath, "//li[@title='Add a Hero Image' and contains(@class,'ui-sortable-handle')]").click
+  find(:xpath, "//li[@title='Add a Hero Image' and contains(@class,'ui-sortable-handle')]//a[@class='block-edit']").click
 end
 
-Then(/^I update the picture "([^"]*)" from hero image$/) do |  image |
+Then(/^I update the hero image block picture$/) do
   # Upload picture to hero block
-  find(:xpath, "//div[@aria-hidden='false']//a[@class='aq_upload_button button']").click()
-  fill_in_visible_element("//input[@id='media-search-input']", image)
+  find(:xpath, "//div[@aria-hidden='false']//a[@class='aq_upload_button button']").click
+  fill_in_visible_element("//input[@id='media-search-input']", $website_configuration['project_items']['hero_image']['image'])
   wait_spinner
   click_on_visible_element("//li[@class='attachment save-ready']")
   click_on_visible_element("//button[text()='Sélectionner']")
 end
 
-And(/^I change the hero title by "([^"]*)"$/) do |  title |
+And(/^I change the hero image block title$/) do
   # Change title hero image
   find(:xpath, "//div[@aria-hidden='false']//label[text()='Intensity for Text and ui elements']").visible?
   execute_script("document.querySelector(\"div[aria-hidden='false'] div.sortable-handle a\",':before').click()")
@@ -73,9 +73,9 @@ And(/^I change the hero title by "([^"]*)"$/) do |  title |
   fill_in("aq_blocks[aq_block_2][tabs][1][subtitle]", with:  '  ')
 end
 
-When(/^I open the thumbnails grid block$/) do | |
-  find(:xpath, "//li[@title='Generate a Thumbnail Grid']").click()
-  find(:xpath, "//li[@title='Generate a Thumbnail Grid']//a[@class='block-edit']").click()
+When(/^I open the thumbnails grid block$/) do
+  find(:xpath, "//li[@title='Generate a Thumbnail Grid']").click
+  find(:xpath, "//li[@title='Generate a Thumbnail Grid']//a[@class='block-edit']").click
 end
 
 Then(/^I open add images tab$/) do
@@ -85,32 +85,26 @@ end
 
 And(/^I clean the images in thumbnails grid$/) do
   images = page.all(:xpath, "//button[@class='button-link attachment-close media-modal-icon']")
-  puts "Cleaning the #{images.length()} images"
+  puts "Cleaning the #{images.length} images"
   images.each { |image|
-    image.click()
+    image.click
   }
   check_text_present('Aucun élément trouvé.')
 end
 
-And(/^I add my imported pictures with legend "([^"]*)"$/) do | legend |
+And(/^I add to the gallery my previously imported pictures with configure legend$/) do
   click_on_visible_element("//a[text()='Ajouter à la galerie']")
   wait_spinner
-  fill_in('media-search-input', with: legend)
+  fill_in('media-search-input', with: $website_configuration['image_import']['legend'])
   wait_spinner
-  images = page.all( :xpath, "//li[@class='attachment save-ready']")
-  puts "#{images.length()} images will be added"
+  images = page.all(:xpath, "//li[@class='attachment save-ready']")
+  puts "#{images.length} images will be added"
 
   images.each { |image|
-    image.click()
+    image.click
   }
   click_on_visible_element("//button[text()='Ajouter à la galerie']")
   click_on_visible_element("//button[text()='Mettre à jour la galerie']")
   click_on_visible_element("//button[text()='Done']")
   click_on_visible_element("//input[@id='save-post']")
 end
-# # And(/^ "([^"]*)"$/) do | |
-
-
-# //button[@class='button-link attachment-close media-modal-icon']
-
-# And(/^ "([^"]*)"$/) do | |
